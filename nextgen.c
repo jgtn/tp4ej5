4160ae003adc7dda2229c6b4d7a3e0e96215f748
jgtn <jgaytan@itba.edu.ar>
2018-04-01 15:03:08 -0300

    Función apta para compilación - warning en pasaje de datos (matriz)

--- nextgen.c
+++ nextgen.c
@@ -1,15 +1,16 @@
 #include  <stdio.h>
 #define P 2
-#define N 2
-#define M 2
+#define N 3
+#define M 3
 #define DEAD 0
 #define ALIVE 1
-
-void nextgen(char world[N][M][P]);  // Declaración de la función nextgen()
+void nextgen(char world[P][N][M]);                        // Declaración de la función nextgen()
+char surroundings_chk(char matriz[P][N][M],char,char);    // Declaración de la función surroundings_chk()
 
 int main(void)
 {
-
+  char matriz[P][N][M] = {{{0,0,0},{0,1,0},{0,0,0}},{{0,0,0},{0,0,0},{0,0,0}}};
+  nextgen(matriz[P][N][M]);
 }
 /***************************************************************************************************
 FUNCION nextgen()
@@ -29,80 +30,83 @@ void nextgen(char world [P][N][M])
   int fila,columna;
   for(fila = 0; fila <= (N-1);fila++)
   {
-    for(columna = 0; columna <= (M-1))
+    for(columna = 0; columna <= (M-1);columna++)
     {
-      char cell,neighbours;                                         // creo variable que indica el contenido de una célula y variable que cuenta la cantidad de "vecinos"
-      cell = world [0][fila][columna];                              // asigno a dicha variable el contenido de la célula
-      neighbours = surroundings_chk(0,fila,columna);                // llamo a función que informa la cantidad de vecinos de la célula
+      char cell,neighbours;                                                       // creo variable que indica el contenido de una célula y variable que cuenta la cantidad de "vecinos"
+      cell = world [0][fila][columna];                                            // asigno a dicha variable el contenido de la célula
+      neighbours = surroundings_chk(world[P][N][M],fila,columna);                // llamo a función que informa la cantidad de vecinos de la célula
 
-      if((cell==ALIVE && (neighbours==2 || neighbours==3)) || (cell==DEAD && neighours==3))
+      if((cell==ALIVE && (neighbours==2 || neighbours==3)) || (cell==DEAD && neighbours==3))
       {
-        world[1][fila][columna] = ALIVE;                             // si la célula está viva y tiene exactamente 2 o 3 vecinos ,o si está muerta y tiene exactamente 3 vecinos, estará viva
+        world[1][fila][columna] = ALIVE;                                         // si la célula está viva y tiene exactamente 2 o 3 vecinos ,o si está muerta y tiene exactamente 3 vecinos, estará viva
       }
 
       else
       {
-        world[1][fila][columna] = DEAD;                               // si la célula está viva y no tiene exactamente 2 o 3 vecinos, o está muerta y no tiene exactamente 3 vecinos, entonces muere
+        world[1][fila][columna] = DEAD;                                          // si la célula está viva y no tiene exactamente 2 o 3 vecinos, o está muerta y no tiene exactamente 3 vecinos, entonces muere
       }
       }
 
     }
   }
 
-/**********************************************************************/
+  /***************************************************************************************************
+  FUNCION surroundings_chk()
 
-char surroundings_chk(char matriz, char fila, char columna)
+  Determina, para una posición dada de un arreglo en 2 dimensiones, cuantas posiciones adyacentes contienen el caracter
+  1, tomando en cuenta las diagonales. La adyacencia en los bordes de la matriz no comprende un salto de fila/columna
+
+  Recibe: - Dirección array de 3 Dimensiones (2 x N x M) (mat 0 = main y mat 1 = buffer)
+
+  Devuelve: variable char con el número de celdas adyacentes que contienen un 1 (máximo 8)
+
+  Requiere: Definición de constantes N (filas matriz) y M (columnas matriz)
+
+  **************************************************************************************************/
+
+char surroundings_chk(char matriz[P][N][M], char fila, char columna)
 {
   char  neighbours = 0;
 
-  if((fila-1))
-}
+  if((fila-1)>=0 && (columna-1)>=0 && matriz[0][fila-1][columna-1] == 1)  // chequeo posición arriba izquierda
+  {
+    neighbours++;
+  }
 
+  if((fila-1)>=0 && (columna+1)>=0 && matriz[0][fila-1][columna+1] == 1)  // chequeo posición arriba derecha
+  {
+    neighbours++;
+  }
 
+  if((fila+1)>=0 && (columna-1)>=0 && matriz[0][fila+1][columna-1] == 1)  // chequeo posición abajo izquierda
+  {
+    neighbours++;
+  }
 
+  if((fila+1)>=0 && (columna+1)>=0 && matriz[0][fila+1][columna+1] == 1)  // chequeo posición abajo derecha
+  {
+    neighbours++;
+  }
 
+  if(fila>=0 && (columna-1)>=0 && matriz[0][fila][columna-1] == 1)  // chequeo posición aizquierda
+  {
+    neighbours++;
+  }
 
+  if((fila-1)>=0 && columna>=0 && matriz[0][fila-1][columna] == 1)  // chequeo posición arriba
+  {
+    neighbours++;
+  }
 
+  if(fila>=0 && (columna+1)>=0 && matriz[0][fila][columna+1] == 1)  // chequeo posición derecha
+  {
+    neighbours++;
+  }
 
+  if((fila+1)>=0 && columna>=0 && matriz[0][fila+1][columna] == 1)  // chequeo posición abajo
+  {
+    neighbours++;
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/***************************************************************************************************
-FUNCION get_cell()
-
-Devuelve el contenido del elemento de un arreglo de 3 dimensiones cuya posición es (i,j,k)
-donde, i = matriz - j = fila - k = columnas
-
-Recibe: - Dirección array de 3 Dimensiones (2 x N x M) (mat 0 = main y mat 1 = buffer)
-        - Coordendas del elemento que se busca (matriz,fila,columna)
-
-Devuelve: - Contenido de la posición (char)
-
-Requiere: Definición de constantes P (número de matriz), N (filas matriz) y M (columnas matriz)
-
-**************************************************************************************************/
-
-char get_cell(char array[P][N][M],unsigned int matriz,unsigned int fila, unsigned int columna)
-{
-
-
+  return neighbours;
 }
